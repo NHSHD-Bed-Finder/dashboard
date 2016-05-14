6ad88f08342718e14f2d22d1f28b4a68eef77d8e
Matt Stibbs <git@stibbsy.co.uk>
2016-05-14 15:03:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -7,5 +9,11 @@ app = Flask(__name__)
 def dashboard():
     return render_template('dashboard.html')
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port = port,
+        debug=True
+    )
